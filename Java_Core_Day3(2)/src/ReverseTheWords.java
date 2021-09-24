b5c3b367dc3214416f7876cb0de3c245f6142fc7
minhler2001@gmail.com <minhler2001@gmail.com>
2021-09-24 16:33:12 +0700

    add new

--- Java_Core_Day3(2)/src/ReverseTheWords.java
+++ Java_Core_Day3(2)/src/ReverseTheWords.java
@@ -3,6 +3,7 @@ public class ReverseTheWords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        input = input.trim();
         for(String words : input.split("\\s")){
             for(int i = words.length()-1; i >= 0; i--){
                 System.out.print(words.charAt(i));
